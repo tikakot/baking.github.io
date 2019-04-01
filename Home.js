dcabc3367ed6319d9a075116798231f9402b2cf8
AinAqilah <48902058+AinAqilah@users.noreply.github.com>
2019-04-01 23:06:13 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,9 @@
+
+//loading
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
+
+
+// transition
