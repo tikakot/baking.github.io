93dfd0ca3677fe4c5dcdde16cb1c125d92ca415f
AinAqilah <48902058+AinAqilah@users.noreply.github.com>
2019-05-01 22:13:35 +0800

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,9 +1,28 @@
+$(document).ready(function(){
+  flag=0;
+  $(".icon").click(function(){
+    if(flag==0)
+    {
+      $(".main").css('box-shadow','inset 0px 0px 0px 30px white,inset 0px -70px 0px 30px white ');
+      $(".main").css('background-color','rgba(0,0,0,0.2)');
+      $(".line").css('background-color','black');
+      $(".m1").animate({top:'40%'},200);
+      $(".m2").animate({top:'40%'},200);
+      $(".m3").animate({top:'40%'},200);
+      $(".m4").animate({top:'40%'},200);
+      flag=1;
+    }
 
-//loading
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden"; // class "loader hidden"
-});
+    else{
+      $(".main").css('box-shadow','inset 0px 0px 0px 0px black,inset 0px 0px 0px 0px black');
+      $(".main").css('background-color','rgba(0,0,0,0)');
+      $(".line").css('background-color','white');
 
-
-// transition
+      $(".m1").animate({top:'100%'},200);
+      $(".m2").animate({top:'100%'},200);
+      $(".m3").animate({top:'100%'},200);
+      $(".m4").animate({top:'100%'},200);
+      flag=0;
+    }
+  })
+})
